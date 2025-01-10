0ab28b873d2918f42cbd37f76721490baafcb5a6
Bee <benicebee@189.cn>
2025-01-10 10:20:36 +0800

    update checkout calling

--- src/build/builderForGithub.groovy
+++ src/build/builderForGithub.groovy
@@ -4,9 +4,8 @@ def buildGolangProject(params) {
         changeLogSets = checkout(changelog: true, poll: false, scm: [
             $class: 'GitSCM',
             branches: [[name: "${params.BUILD_BRANCH}"]],],
-            browser: [$class: 'GithubWeb', repoUrl: "https://github.com/${params['OWNER']}/${params['REPO']}.git"],
             doGenerateSubmoduleConfigurations: false,
-            extensions: [],
+            extensions: [[$class: 'CleanCheckout']],
             submoduleCfg: [],
             userRemoteConfigs: [[url: "ssh://${GIT_USERNAME}@github.com/${params['OWNER']}/${params['REPO']}.git"]]
         )
