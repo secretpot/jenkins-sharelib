5341c5744b874419339a8b6f91b32476fff2a09c
Bee <benicebee@189.cn>
2025-01-10 11:17:20 +0800

    update build process

--- src/build/builderForGithub.groovy
+++ src/build/builderForGithub.groovy
@@ -1,9 +1,5 @@
 def buildGolangProject(params) {
     withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-        echo "OWNER: ${params['OWNER']}"
-        echo "REPO: ${params['REPO']}"
-        echo "BUILD_BRANCH: ${params.BUILD_BRANCH}"
-        // sh "git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${params['OWNER']}/${params['REPO']}.git"
         changeLogSets = checkout(changelog: true, poll: false, scm: [
             $class: 'GitSCM',
             branches: [[name: "${params.BUILD_BRANCH}"]],
@@ -13,7 +9,7 @@ def buildGolangProject(params) {
             userRemoteConfigs: [[url: "https://github.com/${params['OWNER']}/${params['REPO']}.git"]]]
         )
         echo "GIT_COMMIT: ${changeLogSets['GIT_COMMIT']}"
-        sh "go build ."
-        sh "echo built success"
+        sh "scripts/${params['TARGET']}_build.sh"
+        sh "echo built successfully"
     }
 }
