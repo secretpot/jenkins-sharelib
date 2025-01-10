ba574032db6dd4c26073231c3389f65ee490662e
Bee <benicebee@189.cn>
2025-01-10 13:26:04 +0800

    remove build process

--- src/build/builderForGithub.groovy
+++ src/build/builderForGithub.groovy
@@ -1,15 +1,13 @@
-def buildGolangProject(params) {
+def fetchCode(owner, repo, branch) {
     withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
         changeLogSets = checkout(changelog: true, poll: false, scm: [
             $class: 'GitSCM',
-            branches: [[name: "${params.BUILD_BRANCH}"]],
+            branches: [[name: "${branch}"]],
             doGenerateSubmoduleConfigurations: false,
             extensions: [[$class: 'CleanCheckout']],
             submoduleCfg: [],
-            userRemoteConfigs: [[url: "https://github.com/${params['OWNER']}/${params['REPO']}.git"]]]
+            userRemoteConfigs: [[url: "https://github.com/${owner}/${repo}.git"]]]
         )
         echo "GIT_COMMIT: ${changeLogSets['GIT_COMMIT']}"
-        sh "scripts/${params['TARGET']}_build.sh"
-        sh "echo built successfully"
     }
 }
