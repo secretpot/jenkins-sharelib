eb1370a0b52dde53e896f69c0f4cb04caa2bb7f4
Bee <benicebee@189.cn>
2025-01-10 10:35:51 +0800

    fix bug in checkout calling

--- src/build/builderForGithub.groovy
+++ src/build/builderForGithub.groovy
@@ -6,11 +6,11 @@ def buildGolangProject(params) {
         // sh "git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${params['OWNER']}/${params['REPO']}.git"
         changeLogSets = checkout(changelog: true, poll: false, scm: [
             $class: 'GitSCM',
-            branches: [[name: "${params.BUILD_BRANCH}"]],],
+            branches: [[name: "${params.BUILD_BRANCH}"]],
             doGenerateSubmoduleConfigurations: false,
             extensions: [[$class: 'CleanCheckout']],
             submoduleCfg: [],
-            userRemoteConfigs: [[url: "https://github.com/${params['OWNER']}/${params['REPO']}.git"]]
+            userRemoteConfigs: [[url: "https://github.com/${params['OWNER']}/${params['REPO']}.git"]]]
         )
         echo "GIT_COMMIT: ${changeLogSets['GIT_COMMIT']}"
         sh "go build ."
