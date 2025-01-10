f2d44a93803c6b1904aa2bb39f7b8738913e2f3f
Bee <benicebee@189.cn>
2025-01-10 10:26:56 +0800

    add build params echoing

--- src/build/builderForGithub.groovy
+++ src/build/builderForGithub.groovy
@@ -1,5 +1,8 @@
 def buildGolangProject(params) {
     withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+        echo "OWNER: ${params['OWNER']}"
+        echo "REPO: ${params['REPO']}"
+        echo "BUILD_BRANCH: ${params.BUILD_BRANCH}"
         // sh "git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${params['OWNER']}/${params['REPO']}.git"
         changeLogSets = checkout(changelog: true, poll: false, scm: [
             $class: 'GitSCM',
